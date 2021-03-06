6899eb6eb8af5508cf4f03ac08c2d2b8dd5527eb
Debian <debian@debian10.linuxvmimages.local>
2021-03-06 09:07:20 -0600

    Homework

--- fib.py
+++ fib.py
@@ -1,4 +1,3 @@
-
 def CalcFibNum(UserNum):
     if(UserNum <= 100 or UserNum >= 2):
         N1 = 0
@@ -7,28 +6,41 @@ def CalcFibNum(UserNum):
         while(N2 <= UserNum):
             Sum = Sum + N2
             temp = N1 + N2
-            N1 = N2
+            N1 = N2            
             N2 = temp
-        print(Sum)
+        print("The sum of all Fibonacci numbers less than", UserNum, "is equal to:", Sum )    
         return Sum
     else:
         print("Enter a valid number")
         return 1
 
-
 def CheckPrimeNum(PNum):
-    for i in range(3, int(PNum ** 0.5)+ 1):
+    for i in range(2, int(PNum ** 0.5)+ 1):
         if PNum%i == 0:
-            print("Your number is NOT prime")
+            print(PNum, "is NOT a prime number")
             return False
-    print("Your number IS prime")
+    print(PNum, "IS prime")
     return True
 
- main():
+def ConvToBin(DNum):
+    binary = [0,0,0,0,0,0,0,0,]
+    i = len(binary)-1
+    while(DNum > 0):
+        binary[i] = DNum%2
+        i-=1
+        DNum = int(DNum/2)
+    print("The binary representation of your number is equal to: ", end="")
+    for i in binary:
+        print(i, end="")
+    print(end="\n")
+
+
+def main():
     while(True):
         InputNumber = int(input("Please enter a number: "))
         if(CalcFibNum(InputNumber) != 1):
             CheckPrimeNum(InputNumber)
+            ConvToBin(InputNumber)            
             break
 
 main()
