16fa66d444f1b70eea10b71567f6937b3feb17fd
Debian <debian@debian10.linuxvmimages.local>
2021-03-26 06:58:56 -0500

    new files

---
+++ Budget-calculator/main.py
@@ -0,0 +1,34 @@
+{
+        "User": {
+            "Expenses":{
+                    "Electricity": "",
+                    "Water": "",
+                    "Gas": "",
+                    "Groceries": "",
+                    "Eating_out": "",
+                    "Transportation": "",
+                    "Subscriptions": "",
+                    "Hygiene": "",
+                    "Self_care": "",
+                    "Clothing_and_accessories": "",
+                    "Education": "",
+                    "Medicine": "",
+                    "Not_for_yourself": "",
+            }
+            
+            "Calendar": {
+                    "Month": "",
+                    "Income_date": "",
+                    "Payment dates"
+                
+            }
+        }       
+        
+    
+        
+        
+        
+        
+        
+        
+}
